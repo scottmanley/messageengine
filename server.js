f45a5fc566e480dfec391b0a355c2b37e6d37a58
Scott Manley <scott@billycart.net.au>
2014-04-07 22:42:28 +1000

    Added queue service calls

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 #! /usr/local/bin/node --harmony
 
 const
+    azure = require('azure'),
     express = require('express'),
     app = express(),
-    port = process.env.PORT || 1337;;
+    queueName = 'messagequeue',
+    queueService = azure.createQueueService("messagequeue", "B5OuSJlUggdVNGQv6Wm9HFefd/gIYCQc2zwAPUbNKTsSaixNh9TEnMHZLxQt/Efcwd5fYJVTcdnWhG4d4wGOXw=="),
+    port = process.env.PORT || 1337;
 
 app.use(express.logger('dev'));
 app.get('/api/:name', function(req, res) {
-    res.json(200, {
-        "hello": req.params.name
+
+    queueService.peekMessages(queueName, function(error, messages){
+        if(!error){
+            res.json(200, {
+                "hello": messages[0].messagetext
+            });
+        }
     });
+
+
+});
+
+queueService.createQueueIfNotExists(queueName, function(error){
+    if(!error){
+        queueService.createMessage(queueName, "Hello world!", function(error){
+            if(!error){
+                console.log("Message created");
+            }
+        });
+    }
 });
 
 app.listen(port, function() {
